093044f61cfa5d4653725b54e7c2315e7ca0a531
rriecc <s715860428@gmail.com>
2022-09-28 18:34:15 -0400

    add new top bar
    add new top bar

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,48 +1,65 @@
-import { Box } from "@mui/material";
-import { width } from "@mui/system";
+import {Box} from "@mui/material";
+import {width} from "@mui/system";
 import React from "react";
 import Login from "./Login";
+import ResponsiveAppBar from "./TopBar";
+import {blue} from "@mui/material/colors";
 
 function Homepage() {
-  return (
-    <>
-      <Box
-        sx={{
-          marginTop: 8,
-          display: "flex",
-          flexDirection: "row",
-          alignItems: "center",
-        }}
-      >
-        <Box
-          sx={{
-            width: "60%",
-            height: "100%",
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-          }}
-        >
-          <Box sx={{ width: 700, height: 100, border: 1, mb: 5 }} />
-          <Box sx={{ width: 700, height: 400, border: 1 }} />
-        </Box>
+    return (
+        <>
+            <Box
+                sx={{
+                    marginTop: 8,
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                    border: 2,
+                    borderColor: "red"
+                }}
+            >
+                <ResponsiveAppBar/>
+                <Box
+                    sx={{
+                        marginTop: 8,
+                        display: "flex",
+                        flexDirection: "row",
+                        alignItems: "center",
+                        border: 2,
+                        borderColor: "blue"
+                    }}
+                >
 
-        <Box
-          sx={{
-            width: "40%",
-            height: "100%",
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-          }}
-        >
-          <Box sx={{ width: 300, height: 600, padding: 10}}>
-            <Login />
-          </Box>
-        </Box>
-      </Box>
-    </>
-  );
+                    <Box
+                        sx={{
+                            width: "60%",
+                            height: "100%",
+                            display: "flex",
+                            flexDirection: "column",
+                            alignItems: "center",
+                        }}
+                    >
+                        <Box sx={{width: 700, height: 100, border: 1, mb: 5}}/>
+                        <Box sx={{width: 700, height: 400, border: 1}}/>
+                    </Box>
+
+                    <Box
+                        sx={{
+                            width: "40%",
+                            height: "100%",
+                            display: "flex",
+                            flexDirection: "column",
+                            alignItems: "center",
+                        }}
+                    >
+                        <Box sx={{width: 300, height: 600, padding: 10}}>
+                            <Login/>
+                        </Box>
+                    </Box>
+                </Box>
+            </Box>
+        </>
+    );
 }
 
 export default Homepage;
