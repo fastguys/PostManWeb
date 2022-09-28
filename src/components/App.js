cdbfaf39b5a5a5f4bba35be36fd6a07c17a8c2ea
Nanyi-Jiang <jiang731@purdue.edu>
2022-09-28 01:03:56 -0400

    [web] Added Routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,22 @@
+import React from "react";
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+
+import Homepage from './Homepage';
 import SignUp from "./SignUp";
 
+
+
 function App() {
-  return SignUp()
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/signup" element={<SignUp />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
