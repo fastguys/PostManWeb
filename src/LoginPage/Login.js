af07b8456b8098dcc2074a4af11042e5397626b4
Nanyi-Jiang <jiang731@purdue.edu>
2022-10-09 20:46:37 -0400

    [OAuth] added some formatting

--- src/LoginPage/Login.js
+++ src/LoginPage/Login.js
@@ -171,33 +171,33 @@ export default function Login() {
         </Box>
 
         {/* Third party */}
-        <Box
-          sx={{
-            display: "flex",
-            flexDirection: "row",
-            alignItems: "center",
-            justifyContent: "center",
-            mt: 2,
-          }}
-        >
-            {/* Added Microsoft OAuth */}
-            <Button
-              type="submit"
-              onClick={() => {
-                handleMicrosoftLogin();
-              }}
-              startIcon={<img src={"./Microsoft.svg"} alt="microsoft" />}
-            />
-            {/* TODO: Added Google OAuth */}
-            {/* Added Meta OAuth */}
-            <Button
-              type="submit"
-              onClick={() => {
-                handleFacebookLogin();
-              }}
-              startIcon={<img src={"./Meta.svg"} alt="facebook" />}
-            />
-        </Box>
+        <div>
+          {/* Added Microsoft OAuth */}
+          <Button
+            type="submit"
+            variant="outlined"
+            size="small"
+            onClick={() => {
+              handleMicrosoftLogin();
+            }}
+            startIcon={<img src={"./Microsoft.svg"} alt="microsoft" />}
+            sx={{ mx: 1, my: 1}}
+          >Continue with Microsoft</Button>
+        </div>
+          {/* TODO: Added Google OAuth */}
+        <div>
+          {/* Added Meta OAuth */}
+          <Button
+            type="submit"
+            variant="outlined"
+            size="small"
+            onClick={() => {
+              handleFacebookLogin();
+            }}
+            startIcon={<img src={"./Meta.svg"} alt="facebook" />}
+            sx={{ mx: 1, my: 1}}
+          >Continue with Facebook</Button>
+        </div>
       </Box>
     </div>
   );
