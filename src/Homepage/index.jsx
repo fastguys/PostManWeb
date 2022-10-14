f973e38853f7967b51368b7dea50cc0793c4dabb
rriecc <s715860428@gmail.com>
2022-10-14 01:59:25 -0400

    debug

--- src/Homepage/index.jsx
+++ src/Homepage/index.jsx
@@ -1,4 +1,25 @@
+import { useEffect, useState } from "react";
+import { Navigate } from "react-router-dom";
 const HomePage = () => {
-  return (<div>Homepage</div>);
+  const [authenticated, setauthenticated] = useState(null);
+  useEffect(() => {
+    const loggedInUser = localStorage.getItem("authenticated");
+    if (loggedInUser) {
+      setauthenticated(loggedInUser);
+    }
+
+  }, []);
+
+  if (!authenticated) {
+    console.log("bbbbb")
+    return <Navigate to="/" replace={true}/>;
+  } else {
+    console.log("cccc")
+    return (
+        <div>
+          <p>Welcome to your Homepage</p>
+        </div>
+    );
+  }
 }
 export default HomePage;
