969185caefec7f00aa3cb776de99950347c41128
rriecc <s715860428@gmail.com>
2022-10-22 20:42:07 -0400

    [chat] Add chat function
    [chat] Add chat function

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,18 @@
-const express = require("express");
-const app = express();
-const port = 3000;
+const app = require("express")();
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
+const port = process.env.PORT || 3000;
 
-const connectDB = require("./config/db");
-connectDB();
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.sendFile(__dirname + "/index.html");
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+io.on("connection", (socket) => {
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+  });
+});
+
+http.listen(port, () => {
+  console.log(`Socket.IO server running at http://localhost:${port}/`);
 });
