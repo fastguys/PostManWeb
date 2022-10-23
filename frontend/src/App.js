794ddd7a54d27759f8850f1c2930632e7ac241ed
rriecc <s715860428@gmail.com>
2022-10-22 21:37:49 -0400

    [fix merge error with bx]
    [fix merge error with bx]

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 
-import LoginPage from './LoginPage';
-import SignUpPage from "./SignupPage/SignUpPage";
-import HomePage from "./Homepage";
+import LoginPage from "./views/LoginPage";
+import SignUpPage from "./views/SignupPage/SignUpPage";
+import HomePage from "./views/Homepage";
+import ChatPage from "./views/ChatPage";
 
 import "./App.css";
-
 
 function App() {
   return (
@@ -16,6 +16,7 @@ function App() {
           <Route path="/" element={<LoginPage />} />
           <Route path="/signup" element={<SignUpPage />} />
           <Route path="/homepage" element={<HomePage />} />
+          <Route path="/chatpage" element={<ChatPage />} />
         </Routes>
       </BrowserRouter>
     </div>
