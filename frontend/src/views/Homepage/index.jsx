83f9e087034d80ceec1418028ad77404f7cec745
Nanyi-Jiang <jiang731@purdue.edu>
2022-10-28 00:04:33 -0400

    [web] redesign homepage component

--- frontend/src/views/Homepage/index.jsx
+++ frontend/src/views/Homepage/index.jsx
@@ -1,61 +1,21 @@
-import { useEffect, useState } from "react";
-import { Navigate } from "react-router-dom";
-import ResponsiveAppBar from "../TopBar/TopBar";
-import "./homepage.css"
+import { useEffect, useState } from 'react';
+import { Navigate } from 'react-router-dom';
+import ResponsiveAppBar from '../TopBar/TopBar';
+import TaskTakerPanel from './TaskTakerPanel/index';
+import './homepage.css';
+
 const HomePage = () => {
-  //
-  const [searchLocationBox, setSearchLocationBox] = useState("");
-  const [searchLocation, setSearchLocation] = useState("");
+  const [isTaskTakerMode, setIsTaskTakerMode] = useState(true);
 
-  // handle the search location box change
-  const handleChange = (event) => {
-    setSearchLocationBox(event.target.value);
-  };
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    console.log("searchLocationBox", searchLocationBox);
-    // set the search location
-    setSearchLocation(searchLocationBox);
-    setSearchLocationBox("");
-  };
-
-  // handle the search location, TODO: need to call google map API later
-  useEffect(() => {
-    if (searchLocation) {
-      console.log("searchLocation", searchLocation);
-    }
-    // console.log("searchLocation", searchLocation);
-  }, [searchLocation]);
-
-
-
-
-
-
-  if (!localStorage.getItem("authenticated")) {
-    return <Navigate to="/" replace={true}/>;
+  if (!localStorage.getItem('authenticated')) {
+    return <Navigate to="/" replace={true} />;
   } else {
     return (
-        <div>
-          <ResponsiveAppBar />
-          <div className="task-taker-panel">
-            <div className="task-taker-title">
-              <div>Task Taker Page</div>
-            </div>
-            <div className="task-taker-panel-left">
-              <div className="task-taker-search-bar">
-                <form onSubmit={handleSubmit} >
-                  <input type="text" value={searchLocationBox} onChange={handleChange} placeholder="Enter address you want to search for task..." className="task-taker-search-input"/>
-                  <input type="submit" value="Submit" />
-                </form>
-              </div>
-              <div className="task-taker-search-map">
-                <div>Map</div>
-              </div>
-            </div>
-          </div>
-        </div>
+      <div>
+        <ResponsiveAppBar />
+        {isTaskTakerMode ? <TaskTakerPanel /> : <div>Task Poster Panel</div>}
+      </div>
     );
   }
-}
+};
 export default HomePage;
