9dbd7a0179c06fe82cbe10f123e86231f8bd493b
bx <edlislbx@yeah.net>
2022-11-01 17:50:56 -0400

    [chat] Solved redux bug

--- frontend/src/stores/index.js
+++ frontend/src/stores/index.js
@@ -1,8 +1,34 @@
 import { configureStore } from '@reduxjs/toolkit';
 import chatReducer from './chat';
+import storage from 'redux-persist/lib/storage';
+import { combineReducers } from 'redux';
+import {
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+} from 'redux-persist';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const reducers = combineReducers({
+  chat: chatReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, reducers);
 
 export default configureStore({
-  reducer: {
-    chat: chatReducer,
-  }
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware({
+          serializableCheck: {
+              ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+          },
+      }),
 });
