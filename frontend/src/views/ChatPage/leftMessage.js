2ef63ee51f343e54d9e4248415a4766b72e65a53
rriecc <s715860428@gmail.com>
2022-11-01 13:06:21 -0400

    [chat] add profile pic in chatpage

--- frontend/src/views/ChatPage/leftMessage.js
+++ frontend/src/views/ChatPage/leftMessage.js
@@ -1,23 +1,49 @@
 import { Box } from "@mui/system";
+import IconButton from "@mui/material/IconButton";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
+import ListItemAvatar from "@mui/material/ListItemAvatar";
+import Avatar from "@mui/material/Avatar";
+import * as React from "react";
+import apis from "../../apis/user";
 
 function LeftMessage({ message }) {
+  const [image, setImage] = React.useState(null);
+  let email = localStorage.getItem("userId");
+  React.useEffect(() => {
+    apis.FinduserByEmail({ email }).then((res) => {
+      setImage(res[0].ImageUrl);
+    });
+  }, [image]);
   return (
-    <Box
+    <List
       sx={{
-        padding: 2,
-        margin: 2,
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "lightblue",
-        borderRadius: 2,
         alignSelf: "flex-end",
-        marginRight: 5,
+        bgcolor: "background.paper",
       }}
     >
-      {message}
-    </Box>
+      <ListItem alignItems="flex-start">
+        <Box
+          sx={{
+            padding: 2,
+            margin: 2,
+            display: "flex",
+            justifyContent: "center",
+            backgroundColor: "lightblue",
+            borderRadius: 2,
+          }}
+        >
+          {message}
+        </Box>
+        <ListItemAvatar sx={{ alignItems: "flex-start" }}>
+          <Avatar
+            alt="Remy Sharp"
+            src={image ? image : "/static/images/avatar/2.jpg"}
+          />
+        </ListItemAvatar>
+      </ListItem>
+    </List>
   );
 }
 
