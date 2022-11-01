bab2719c8f9952d304330e8280fa9476f59f950f
rriecc <s715860428@gmail.com>
2022-11-01 13:57:19 -0400

    [chat] add profile pic in chatpage and fix bug

--- frontend/src/views/ChatPage/leftMessage.js
+++ frontend/src/views/ChatPage/leftMessage.js
@@ -1,41 +1,39 @@
-import { Box } from '@mui/system';
-import IconButton from '@mui/material/IconButton';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemText from '@mui/material/ListItemText';
-import ListItemAvatar from '@mui/material/ListItemAvatar';
-import Avatar from '@mui/material/Avatar';
-import * as React from 'react';
-import apis from '../../apis/user';
+import { Box } from "@mui/system";
+import IconButton from "@mui/material/IconButton";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
+import ListItemAvatar from "@mui/material/ListItemAvatar";
+import Avatar from "@mui/material/Avatar";
+import * as React from "react";
+import apis from "../../apis/user";
 
-function LeftMessage({ message }) {
-  const [image, setImage] = React.useState(null);
-  let email = localStorage.getItem('userId');
-  React.useEffect(() => {
-    apis.FinduserByEmail({ email }).then((res) => {
-      setImage(res[0].ImageUrl);
-    });
-  }, [image]);
+function LeftMessage({ message, image }) {
   return (
     <List
       sx={{
-        alignSelf: 'flex-end',
-        bgcolor: 'background.paper'
-      }}>
+        alignSelf: "flex-end",
+        bgcolor: "background.paper",
+      }}
+    >
       <ListItem alignItems="flex-start">
         <Box
           sx={{
             padding: 2,
             mx: 2,
-            display: 'flex',
-            justifyContent: 'center',
-            backgroundColor: 'lightblue',
-            borderRadius: 2
-          }}>
+            display: "flex",
+            justifyContent: "center",
+            backgroundColor: "lightblue",
+            borderRadius: 2,
+          }}
+        >
           {message}
         </Box>
-        <ListItemAvatar sx={{ alignItems: 'flex-start', my: 0.5 }}>
-          <Avatar alt="Remy Sharp" src={image ? image : '/static/images/avatar/2.jpg'} />
+        <ListItemAvatar sx={{ alignItems: "flex-start", my: 0.5 }}>
+          <Avatar
+            alt="Remy Sharp"
+            src={image ? image : "/static/images/avatar/2.jpg"}
+          />
         </ListItemAvatar>
       </ListItem>
     </List>
