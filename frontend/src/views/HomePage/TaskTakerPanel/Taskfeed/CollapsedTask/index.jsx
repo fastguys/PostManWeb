515f0b714a6fd8c8e111f27a67471f0290628b04
Nanyi-Jiang <jiang731@purdue.edu>
2022-11-01 15:18:13 -0400

    [pull] minor adjustment

--- frontend/src/views/HomePage/TaskTakerPanel/Taskfeed/CollapsedTask/index.jsx
+++ frontend/src/views/HomePage/TaskTakerPanel/Taskfeed/CollapsedTask/index.jsx
@@ -1,22 +1,22 @@
-import React from 'react';
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import List from '@mui/material/List';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import Collapse from '@mui/material/Collapse';
-import CheckCircleIcon from '@mui/icons-material/CheckCircle';
-import IconButton from '@mui/material/IconButton';
-import TaskIcon from '@mui/icons-material/Task';
-import UnpublishedIcon from '@mui/icons-material/Unpublished';
-import ChatIcon from '@mui/icons-material/Chat';
-import SpeakerNotesOffIcon from '@mui/icons-material/SpeakerNotesOff';
-import ExpandLess from '@mui/icons-material/ExpandLess';
-import ExpandMore from '@mui/icons-material/ExpandMore';
-import DescriptionIcon from '@mui/icons-material/Description';
-import { ListItem, ListItemSecondaryAction } from '@mui/material';
-import apis from '../../../../../apis/user';
+import React from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import List from "@mui/material/List";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import Collapse from "@mui/material/Collapse";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
+import IconButton from "@mui/material/IconButton";
+import TaskIcon from "@mui/icons-material/Task";
+import UnpublishedIcon from "@mui/icons-material/Unpublished";
+import ChatIcon from "@mui/icons-material/Chat";
+import SpeakerNotesOffIcon from "@mui/icons-material/SpeakerNotesOff";
+import ExpandLess from "@mui/icons-material/ExpandLess";
+import ExpandMore from "@mui/icons-material/ExpandMore";
+import DescriptionIcon from "@mui/icons-material/Description";
+import { ListItem, ListItemSecondaryAction } from "@mui/material";
+import apis from "../../../../../apis/user";
 
 const CollapsedTask = (props) => {
   const [open, setOpen] = useState(false);
@@ -33,34 +33,34 @@ const CollapsedTask = (props) => {
   const handleTaskTakeClick = (e) => {
     // check if the task is taken
     if (taskInfo.isTaken === true) {
-      console.log('task is taken');
+      console.log("task is taken");
       return;
     }
     // set the task taken
     taskInfo.isTaken = true;
-    taskInfo.takerId = localStorage.getItem('userId');
-    taskInfo.status = 'taken';
+    taskInfo.takerId = localStorage.getItem("userId");
+    taskInfo.status = "taken";
     // TODO: update the status in the database
     apis.UpdateTask(taskInfo._id, taskInfo).then((res) => {
-      console.log('res', res);
+      console.log("res", res);
     });
 
     props.setTaskTaken((taskTaken) => [...taskTaken, taskInfo]);
     // redirect to task progress page
-    navigate('/task-progress');
+    navigate("/task-progress");
   };
 
   // controller for the task chatting button
   const handleTaskChatClick = (e) => {
     // check if the task is taken
     if (taskInfo.isTaken === true) {
-      console.log('task is taken');
+      console.log("task is taken");
       return;
     }
     // navigate to the task chatting page with the specific task poster
-    localStorage.setItem('chatWithPosterId', taskInfo.posterId);
-    localStorage.setItem('chatWithTaskId', taskInfo.taskId);
-    navigate('/chatpage');
+    localStorage.setItem("chatWithPosterId", taskInfo.posterId);
+    localStorage.setItem("chatWithTaskId", taskInfo.taskId);
+    navigate("/chatpage");
   };
 
   return (
@@ -84,7 +84,11 @@ const CollapsedTask = (props) => {
             <IconButton aria-label="chat" onClick={handleTaskChatClick}>
               {taskInfo.isTaken ? <SpeakerNotesOffIcon /> : <ChatIcon />}
             </IconButton>
-            <IconButton edge="end" aria-label="take" onClick={handleTaskTakeClick}>
+            <IconButton
+              edge="end"
+              aria-label="take"
+              onClick={handleTaskTakeClick}
+            >
               {taskInfo.isTaken ? <UnpublishedIcon /> : <CheckCircleIcon />}
             </IconButton>
           </ListItemSecondaryAction>
