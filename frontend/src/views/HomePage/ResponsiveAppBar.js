e6de264da26d1f382ec3da4e890fdb15a8dc9b71
rriecc <s715860428@gmail.com>
2022-11-03 22:15:32 -0400

    [homepage] add chat button

--- frontend/src/views/HomePage/ResponsiveAppBar.js
+++ frontend/src/views/HomePage/ResponsiveAppBar.js
@@ -1,22 +1,22 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
-import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import Tooltip from '@mui/material/Tooltip';
-import MenuItem from '@mui/material/MenuItem';
-import AdbIcon from '@mui/icons-material/Adb';
-import SegmentedControl from 'mui-segmented-control';
-import { useNavigate } from 'react-router-dom';
-import apis from '../../apis/user';
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import Tooltip from "@mui/material/Tooltip";
+import MenuItem from "@mui/material/MenuItem";
+import AdbIcon from "@mui/icons-material/Adb";
+import SegmentedControl from "mui-segmented-control";
+import { useNavigate } from "react-router-dom";
+import apis from "../../apis/user";
 const pages = [];
-const settings = ['Profile', 'Logout'];
+const settings = ["Profile", "Logout", "myChat"];
 
 export default function ResponsiveAppBar(props) {
   const navigate = useNavigate();
@@ -26,7 +26,7 @@ export default function ResponsiveAppBar(props) {
 
   const [value, setValue] = React.useState(1);
   const [image, setImage] = React.useState(null);
-  let email = localStorage.getItem('userId');
+  let email = localStorage.getItem("userId");
   apis.FinduserByEmail({ email }).then((res) => {
     if (res[0] && res[0].ImageUrl) {
       setImage(res[0].ImageUrl);
@@ -52,18 +52,21 @@ export default function ResponsiveAppBar(props) {
   };
   const OpenProfile = (e) => {
     console.log(e);
-    navigate('/profilepage');
+    navigate("/profilepage");
   };
   const logout = () => {
     localStorage.clear();
-    navigate('/');
+    navigate("/");
+  };
+  const gotoChat = () => {
+    navigate("/chatpage");
   };
   const handleCloseUserMenu = () => {
     setAnchorElUser(null);
   };
 
   return (
-    <AppBar position="static" style={{ background: '#656268' }}>
+    <AppBar position="static" style={{ background: "#656268" }}>
       <Container maxWidth="xl">
         <Toolbar disableGutters>
           {/*<img*/}
@@ -79,43 +82,46 @@ export default function ResponsiveAppBar(props) {
             href="/homepage"
             sx={{
               mr: 2,
-              display: { xs: 'none', md: 'flex' },
-              fontFamily: 'monospace',
+              display: { xs: "none", md: "flex" },
+              fontFamily: "monospace",
               fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'inherit',
-              textDecoration: 'none'
-            }}>
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
+            }}
+          >
             PostMan
           </Typography>
 
-          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
             <IconButton
               size="large"
               aria-label="account of current user"
               aria-controls="menu-appbar"
               aria-haspopup="true"
               onClick={handleOpenNavMenu}
-              color="inherit">
+              color="inherit"
+            >
               <MenuIcon />
             </IconButton>
             <Menu
               id="menu-appbar"
               anchorEl={anchorElNav}
               anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left'
+                vertical: "bottom",
+                horizontal: "left",
               }}
               keepMounted
               transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left'
+                vertical: "top",
+                horizontal: "left",
               }}
               open={Boolean(anchorElNav)}
               onClose={handleCloseNavMenu}
               sx={{
-                display: { xs: 'block', md: 'none' }
-              }}>
+                display: { xs: "block", md: "none" },
+              }}
+            >
               {pages.map((page) => (
                 <MenuItem key={page} onClick={handleCloseNavMenu}>
                   <Typography textAlign="center">{page}</Typography>
@@ -131,22 +137,24 @@ export default function ResponsiveAppBar(props) {
             href=""
             sx={{
               mr: 2,
-              display: { xs: 'flex', md: 'none' },
+              display: { xs: "flex", md: "none" },
               flexGrow: 1,
-              fontFamily: 'monospace',
+              fontFamily: "monospace",
               fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'inherit',
-              textDecoration: 'none'
-            }}>
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
+            }}
+          >
             PostMan
           </Typography>
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
             {pages.map((page) => (
               <Button
                 key={page}
                 onClick={handleCloseNavMenu}
-                sx={{ my: 2, color: 'white', display: 'block' }}>
+                sx={{ my: 2, color: "white", display: "block" }}
+              >
                 {page}
               </Button>
             ))}
@@ -157,13 +165,13 @@ export default function ResponsiveAppBar(props) {
             color="primary"
             options={[
               {
-                label: 'TaskTaker',
-                value: 1
+                label: "TaskTaker",
+                value: 1,
               },
               {
-                label: 'TaskPoster',
-                value: 2
-              }
+                label: "TaskPoster",
+                value: 2,
+              },
             ]}
             value={value}
             onChange={setValue}
@@ -173,26 +181,39 @@ export default function ResponsiveAppBar(props) {
           <Box sx={{ flexGrow: 0, ml: 4 }}>
             <Tooltip title="Open settings">
               <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src={image ? image : '/static/images/avatar/2.jpg'} />
+                <Avatar
+                  alt="Remy Sharp"
+                  src={image ? image : "/static/images/avatar/2.jpg"}
+                />
               </IconButton>
             </Tooltip>
             <Menu
-              sx={{ mt: '45px' }}
+              sx={{ mt: "45px" }}
               id="menu-appbar"
               anchorEl={anchorElUser}
               anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right'
+                vertical: "top",
+                horizontal: "right",
               }}
               keepMounted
               transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right'
+                vertical: "top",
+                horizontal: "right",
               }}
               open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}>
+              onClose={handleCloseUserMenu}
+            >
               {settings.map((setting) => (
-                <MenuItem key={setting} onClick={setting === 'Profile' ? OpenProfile : logout}>
+                <MenuItem
+                  key={setting}
+                  onClick={
+                    setting === "Profile"
+                      ? OpenProfile
+                      : setting == "Logout"
+                      ? logout
+                      : gotoChat
+                  }
+                >
                   <Typography textAlign="center">{setting}</Typography>
                 </MenuItem>
               ))}
