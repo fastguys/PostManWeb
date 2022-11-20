c4d742758642406a27ca35bbb2f2e9481209f389
rriecc <s715860428@gmail.com>
2022-11-19 22:29:42 -0500

    [MAP] Add map feature to task poster and task takerpage

---
+++ frontend/src/views/HomePage/Map.js
@@ -0,0 +1,37 @@
+import { Box } from "@mui/system";
+import {
+  useJsApiLoader,
+  GoogleMap,
+  Marker,
+  Autocomplete,
+  DirectionsRenderer,
+} from "@react-google-maps/api";
+const center = { lat: 48.8584, lng: 2.2945 };
+
+function Map() {
+  //MAPS API
+  const { isLoaded } = useJsApiLoader({
+    googleMapsApiKey: "AIzaSyDHcel8Zif6__KnyYRvsxHCIELH4kCRTTA",
+  });
+
+  if (!isLoaded) {
+    return <div>Loading...</div>;
+  }
+  return (
+    <Box
+      sx={{
+        height: "100%",
+        width: "100%",
+        bgcolor: "primary.main",
+        borderRadius: 2,
+      }}
+    >
+      <GoogleMap
+        center={center}
+        zoom={10}
+        mapContainerStyle={{ width: "100%", height: "100%" }}
+      ></GoogleMap>
+    </Box>
+  );
+}
+export default Map;
